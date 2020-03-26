f66b45f8e1a6266bdd0d74bf200b125a2b612454
kippeinakade <tkrs23280810@gmail.com>
2020-03-26 13:30:11 +0000

    add devise&post controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+  def index
+  	@posts = Post.all
+  end
+
+  def show
+  	@post = Post.find(params[:id])
+  end
+
+  def new
+  	@post = Post.new
+  end
+
+  def create
+  	@post = Post.new(post_params)
+  	@post.user_id = current_user.id
+  	if @post.save
+  		redirect_to posts_path
+  	else
+  		render 'new'
+  	end
+  end
+end
+
+private
+  def post_params
+  	params.require(:post).permit(:title, :body)
+  end
