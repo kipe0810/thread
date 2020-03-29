57c74f87e5e512c97ade63d08be8d2d9d122ca17
kippeinakade <tkrs23280810@gmail.com>
2020-03-29 06:36:25 +0000

    search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,7 @@ class SearchController < ApplicationController
 
 	def search
 		@posts = Post.search(params[:search])
+		# @comment = Post.includes(:post_comments).where(params[:search])
 		@search = params[:search]
 	end
 end
--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -1,4 +1,12 @@
 class PostComment < ApplicationRecord
 	belongs_to :user
 	belongs_to :post
+
+	def self.search(search)
+		if search
+			PostComment.where(['comment LIKE ?', "%#{search}%"])
+		else
+			PostComment.all
+		end
+	end
 end
