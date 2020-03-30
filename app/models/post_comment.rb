020ef6a9c85f35d9eb2d7b2ad579d790818354a7
kippeinakade <tkrs23280810@gmail.com>
2020-03-30 09:30:11 +0000

    model/comment

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -1,12 +1,4 @@
 class PostComment < ApplicationRecord
 	belongs_to :user
 	belongs_to :post
-
-	def self.search(search)
-		if search
-			PostComment.where(['comment LIKE ?', "%#{search}%"])
-		else
-			PostComment.all
-		end
-	end
 end
